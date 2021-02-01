6c269df3b9b5ffe45d7eb601b40bb3d949a3f59e
Ryan Vreeland <ravreeland@smcm.edu>
2021-02-01 11:21:31 -0500

    "Added second piece of data for mixed messages"

--- MixedMessagesApplication.js
+++ MixedMessagesApplication.js
@@ -15,14 +15,41 @@ const randomAnnouncementWords = () => {
         "Listen!",
         "Gather 'round!",
         "Listen, n gather 'round!",
-        "Come and look!"
+        "Come and look!",
+        "Newsflash!"
     ]
     const randomIndex = Math.floor(Math.random()*headLineArray.length);
     return headLineArray[randomIndex];
 }
 
+const catchyMiddleSubject= () => {
+    const middleSubjectArray = [
+        "Superman saves plane from crashing,",
+        "Superman kills Luis Lane,",
+        "Crazy man in bat costume thinks reporter is superman,",
+        "Billionaire thinks he has more money than a millionaire,",
+        "Zoo lion escapes and eats icecream,",
+        "Local Florida man in crocodile hat tries to get in crocodile pen,",
+        "Aliens have been found, and are currently taking control over the earth,",
+        "Florida has officially declared being a 'Florida man' is illegal,",
+        "Local woman walks around with a murder of crows, and burns down the patriarchy,",
+        "Naboo politician makes the snarky remark of 'democracy dying to thunderous applause',",
+        "Emperor who 'loves democracy' gets rid of senate,",
+        "Hello there. General Kenobi. You are a bold one,",
+        "Newly appointed Lord chastizes Imperial officer, proceeds to choke him,",
+        "News reporter finds themselves trapped in program,",
+        "Oil executive declares 'war on oil' to increase gas prices,",
+        "First man decides bones are delicious,",
+        "Super powers are now available to trade and buy,",
+    ]
+    const randomIndex = Math.floor(Math.random()*middleSubjectArray.length);
+    return middleSubjectArray[randomIndex];
+}
+
+
 for (let i = 0; i<30; i++){
     console.log(randomAnnouncementWords());
+    console.log(catchyMiddleSubject());
 }
 
 
